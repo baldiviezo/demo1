e49bb7663c09bbc6f79f54b46af9491cf8bd0cfe
baldiviezo <baldiviezo.info@gmail.com>
2023-09-18 15:05:37 -0400

    demo1

--- vistas/scripts/sesionIniciada.js
+++ vistas/scripts/sesionIniciada.js
@@ -1,15 +1,12 @@
-
-    let objeto = {
-        'id_usua': localStorage.getItem('id_usua')
+let objeto = {
+    'id_usua': localStorage.getItem('id_usua')
+}
+objeto = JSON.stringify(objeto);
+fetch('../sesionIniciada', {
+    method: "POST",
+    body: objeto
+}).then(response => response.text()).then(data => {
+    if (data != 1){
+        window.open('./login.html',"_self");
     }
-    objeto = JSON.stringify(objeto);
-    console.log(objeto)
-	fetch('../sesionIniciada', {
-        method: "POST",
-        body: objeto
-    }).then(response => response.text()).then(data => {
-        console.log(data);
-        if (data != 1){
-            window.open('./login.html',"_self");
-        }
-    })
+})
